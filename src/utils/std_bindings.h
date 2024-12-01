f946aaf1a83e5c300650eb05ae25249118c6142a
Mateusz Kulig <mateusz.kulig02@gmail.com>
2024-12-01 23:17:00 +0100

    Change style of comment

--- src/utils/std_bindings.h
+++ src/utils/std_bindings.h
@@ -1,9 +1,12 @@
 #ifndef STD_BINGINGS_H
 #define STD_BINGINGS_H
 
-// Defininions of std libc functions to use
-// Change if in need to use custom funcitons instead of standard ones
 
+/**
+ * @brief Defininions of std libc functions to use, change if in need to use custom funcitons
+ *        instead of standard ones
+ * 
+ */
 #define getCharFromFile(filePointer) fgetc(filePointer)
 
 #endif
